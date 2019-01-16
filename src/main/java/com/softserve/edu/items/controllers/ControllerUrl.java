231c7f9590ec0c60ef58b860017af63ff0da871e
bebebe <hahaha@gmail.com>
2019-01-16 16:10:23 +0200

    mvc project example

---
+++ src/main/java/com/softserve/edu/items/controllers/ControllerUrl.java
@@ -0,0 +1,6 @@
+package com.softserve.edu.items.controllers;
+
+public class ControllerUrl {
+	public static final String ROOT_SERVLET = "/";
+	public static final String LOGIN_SERVLET = "/login";
+}
