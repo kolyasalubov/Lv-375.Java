4cf93cf9b7157e354025e492c10e64e7e583c1a3
Severyn <tsyapkaseveryn@gmail.com>
2019-01-21 05:11:12 +0200

    100%END

--- src/main/java/myArticles/edu/Main.java
+++ src/main/java/myArticles/edu/Main.java
@@ -1,14 +1,9 @@
 package myArticles.edu;
 
 
-import myArticles.edu.Services.UserArticlesService;
-import myArticles.edu.container.IocContainer;
-import myArticles.edu.dto.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println((String)System.getenv().get("MY_SQL_LOGIN"));
-        System.out.println((String)System.getenv().get("MY_SQL_PASSWORD"));
+
     }
 }
