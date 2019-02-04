3f9cabf01424abd5978aa9d26638c1968501b12e
bebebe <hahaha@gmail.com>
2019-02-04 13:57:26 +0200

    Threading Examples

---
+++ src/main/java/com/softserve/edu/Appl.java
@@ -0,0 +1,10 @@
+package com.softserve.edu;
+
+public class Appl {
+
+    public static void main(String[] args) {
+        DialogTime dt = new DialogTime();
+        dt.setVisible(true);
+    }
+    
+}
